6a1c939f96fd831f4eaebd6ce04b678a9b61a82a
Thaadshaayani Rasanehru <thaadshaayanirasanehru@gmail.com>
2025-11-29 21:09:53 +0400

    Update smart_router.py

--- core/smart_router.py
+++ core/smart_router.py
@@ -3,6 +3,8 @@
 import re
 from .intent_classifier import classify_query_intent
 from .lifestyle_rag_v2 import try_build_lifestyle_card
+from .lifestyle_rag_v2 import _looks_like_lifestyle_query
+
 
 
 # Clustering (safe to import at top)
